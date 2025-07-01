632ebc4bc6a915f0c9b399646ff774677e2660c1
ishu <sushantbharti197@gmail.com>
2025-07-01 11:18:41 +0530

    project Completed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { useChatStore } from '../store/useChatStore'
 import ChatContainer from '../components/ChatContainer'
 import NoChatSelected from '../components/NoChatSelected'
-import Sidebar from '../components/sidebar'
+import Sidebar from '../components/Sidebar'
 
 
-const HomePage = () => {
-  const{selectedUser} = useChatStore();
+
+function HomePage() {
+  const { selectedUser } = useChatStore()
 
   return (
     <div className="h-screen bg-base-200">
